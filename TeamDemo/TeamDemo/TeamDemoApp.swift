7506c6ebc261b5a13349b9dbfa38e8484d2d334a
wongbingg <wb277@naver.com>
2025-07-07 19:02:04 +0900

    initial commit

---
+++ TeamDemo/TeamDemo/TeamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamDemoApp.swift
+//  TeamDemo
+//
+//  Created by 이원빈 on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TeamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
